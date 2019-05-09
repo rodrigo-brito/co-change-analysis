acfd930a483f9037042945fdca4a9ef2ba95d370
davidoro <davi_doro@hotmail.com>
2019-05-09 15:28:10 -0300

    Summary - New option -t for a transactions file that contains all the transactions to be analised by the apriori (therefore skiping cloning and parsing). - New option -ot for outputing transactions file that contains all the transactions that will be fetched (either from a remote remo entered with -r or local repo entered with -d) - Argument --lang is no longer required, since parsing can be skipped if --transactions is supplied. - New import: json

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 import git
 import argparse
+import json
 
 from pydriller import RepositoryMining
 
@@ -18,11 +19,12 @@ from pprint import pprint
 p = argparse.ArgumentParser(description="Prepare something code.")
 p.add_argument("-r", "--repository", help="Remote Git repository", required=False)
 p.add_argument("-d", "--dir", help="Local project directory", required=False)
-p.add_argument("-l", "--lang", help="Programming language", choices=['python', 'java', 'go'], required=True)
+p.add_argument("-l", "--lang", help="Programming language", choices=['python', 'java', 'go'])
 p.add_argument("-s", "--support", help="correlation support value", default=0.5)
 p.add_argument("-c", "--confidence", help="correlation support value", default=0.5)
 p.add_argument("-L", "--max_length", help="max number of items in a rule", default=2)
 p.add_argument("-t", "--transactions", help="transactions file", required=False)
+p.add_argument("-ot", "--out_transactions", help="write transactions file", required=False)
 
 def main():
     args = p.parse_args()
@@ -35,41 +37,50 @@ def main():
     print("confidence=%.3f" % (confidence))
     print("max_length=%d" % (max_length))
     
-    gitpy = git.Git(CLONE_PATH)
-
-    # fetching repository / folder
-    project_path = args.dir
-    if args.repository:
-        project_folder = "clone-%s" % generate_hash()
-        project_path = "%s/%s" % (CLONE_PATH, project_folder)
-        print("Cloning repository to %s..." % project_path)
-        gitpy.clone(args.repository, project_folder)
-
-    print("number of commits: %d" % (get_commit_count(project_path)))
-    print("support absolute value: %d" % (support * get_commit_count(project_path)))
-    
-    # defining language parser
-    parser = BaseParser
-    if args.lang == "go":
-        parser = GoParser
-    elif args.lang == "java":
-        parser = JavaParser
-    elif args.lang == "python":
-        parser = PythonParser
-    
-    print("fetching transactions...")
-    
     transactions = []
-    for commit in RepositoryMining(project_path).traverse_commits():
-        language_parser = parser(project_path, commit.hash)
-        items = language_parser.get_diff()
-        if items:
-            transactions.append(items)
+    
+    if transactions_file_name is None:
+        gitpy = git.Git(CLONE_PATH)
+        
+        # fetching repository / folder
+        project_path = args.dir
+        if args.repository:
+            project_folder = "clone-%s" % generate_hash()
+            project_path = "%s/%s" % (CLONE_PATH, project_folder)
+            print("Cloning repository to %s..." % project_path)
+            gitpy.clone(args.repository, project_folder)
+        
+        print("number of commits: %d" % (get_commit_count(project_path)))
+        print("support absolute value: %d" % (support * get_commit_count(project_path)))
+        
+        # defining language parser
+        parser = BaseParser
+        if args.lang == "go":
+            parser = GoParser
+        elif args.lang == "java":
+            parser = JavaParser
+        elif args.lang == "python":
+            parser = PythonParser
+        
+        print("fetching transactions...")
+        
+        for commit in RepositoryMining(project_path).traverse_commits():
+            language_parser = parser(project_path, commit.hash)
+            items = language_parser.get_diff()
+            if items:
+                transactions.append(items)
+    else:
+        transaction_json = open(transactions_file_name, 'r').read()
+        transactions = json.loads(transaction_json)
         
     print(ansi_color_yellow("Transactions:"))
     for i, changes in enumerate(transactions):
         print("%3d: " % (i), end='')
         print(changes)
+        
+    if args.out_transactions:
+        out_file = open(args.out_transactions, 'w')
+        json.dump(transactions, out_file, indent=2)
     
     print("analyzing transactions...")
     
