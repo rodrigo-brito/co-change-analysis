1b981f2b50573e667c99111fa9675c2781357966
davidoro <davi_doro@hotmail.com>
2019-05-08 22:44:03 -0300

    Summary
    - Fixed output from BaseParser.get_diff(...)
    - Added optional max_length parameter, which is the max number of items in a rule
    - Fixed print(...) arguments order
    - Added 'local/' to .gitignore
    - Added function get_commit_count(...)
    - Printing absolute support value
    
    Since the output of BaseParser.get_diff(...) is used to form the input of Apriori.get_rules(), the
    output of get_diff(...) was incorrect. `get_rules()` expects a 'list of lists', but get_diff(...)
    returns a 'list of list of tuples' like this:
    
    [
      ('[doc.go] main',),
      ('[mux.go] main', '[mux.go] init', '[mux.go] setVars', '[mux.go] setCurrentRoute', '[mux.go] mapFromPairs'),
      ('[regexp.go] newRouteRegexp', '[regexp.go] braceIndices')
    ]
    
    We cannot group changes in the same file in a tuple. The get_rules(...) would interpret each
    tuple in this list as being an item of the transaction, but as you can see the second item contains
    in fact multiple items (that happened in the same file, but multiple transactions nonetheless).
    The correct input of a single transaction for get_rules for this same example is this:
    
    [
      '[doc.go] main',
      '[mux.go] main',
      '[mux.go] init',
      '[mux.go] setVars',
      '[mux.go] setCurrentRoute',
      '[mux.go] mapFromPairs',
      '[regexp.go] newRouteRegexp',
      '[regexp.go] braceIndices'
    ]

--- apriori.py
+++ apriori.py
@@ -1,18 +1,21 @@
 from efficient_apriori import apriori
 
-
 class Apriori:
-    def __init__(self, transactions, support=0.5, confidence=0.5):
+    def __init__(self, transactions, support=0.5, confidence=0.5, max_length=8):
         self.transactions = transactions
         self.support = support
         self.confidence = confidence
+        self.max_length = max_length
 
     def set_support(self, support):
         self.support = support
 
     def set_confidence(self, confidence):
         self.confidence = confidence
+        
+    def set_max_length(self, max_length):
+        self.max_length = max_length
 
     def get_rules(self):
-        _, rules = apriori(self.transactions, min_support=self.support, min_confidence=self.confidence)
+        _, rules = apriori(self.transactions, min_support=self.support, min_confidence=self.confidence, max_length=self.max_length)
         return rules
--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,13 @@
 import hashlib
 import os
+from pydriller import RepositoryMining
 
 
 def generate_hash(size=8):
     return hashlib.md5(os.urandom(32)).hexdigest()[:size]
+
+def get_commit_count(project_path):
+    result = 0
+    for commit in RepositoryMining(project_path).traverse_commits():
+        result += 1
+    return result
